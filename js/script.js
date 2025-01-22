60f83789b81185bf13df1aa03249a0de18c25b48
Helinks <130001483+Helinks@users.noreply.github.com>
2025-01-21 23:49:25 -0500

    Commit 2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 let hideBtn = document.getElementById('rdMore_btn');
-
+let answers = document.getElementById('answers');
 let hidenText = document.getElementById('hidenText');
+let hidenAnswers = document.getElementById('hideAnswers');
 
 hideBtn.addEventListener('click',toggleText);
+answers.addEventListener('click',toggleAnswers);
 
 function toggleText (){
     hidenText.classList.toggle('show');
@@ -13,4 +15,8 @@ function toggleText (){
     else{
         hideBtn.classList.toggle('showBtn');
     }
+}
+
+function toggleAnswers (){
+    hidenAnswers.classList.toggle('showAnswers')
 }
